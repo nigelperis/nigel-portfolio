cad75f47bbea9946e401c1338d756a51fa807abd
Nigel Peris <nperis27@gmail.com>
2023-06-09 19:20:21 +0530

    added backend server for mail functionality

--- script.js
+++ script.js
@@ -19,4 +19,40 @@ function loadProjects() {
     };
     xhr.send();
   }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const projectsContainer = document.querySelector('#projectsContainer');
+  
+    // Array of projects
+    const projects = [
+      {
+        image: '/images/proj1.jpg',
+        title: 'Vegetable Disease Detection',
+        description: 'Developed a web app with a CNN model using Machine Learning to classify Tomato and Corn Images into 12 diseases and identify their health status',
+        githubLink: 'https://github.com/nigelperis/vegetable-disease-detection'
+      },
+      {
+        image: '/images/proj2.jpg',
+        title: 'Portfolio Site for Ganesh Designmatics Ltd',
+        description: 'Developed a web app with visually appealing gallery, contact pages, and user login functionality with secure email storage.',
+        githubLink: 'https://github.com/nigelperis/SiteForGaneshDesignmaticsLtd'
+      }
+    ];
+    
+    projects.forEach(project => {
+      const projectElement = document.createElement('div');
+      projectElement.classList.add('project');
+      projectElement.innerHTML = `
+        <img src="${project.image}" alt="${project.title}">
+        <h2>${project.title}</h2>
+        <p>${project.description}</p>
+      `;
+      projectElement.addEventListener('click', () => {
+        window.location.href = project.githubLink;
+      });
+      projectsContainer.appendChild(projectElement);
+    });
+    
+  });
+  
   
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const path = require('path');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse the request body
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '')));
+
+// POST endpoint for the contact form submission
+app.post('/contact', (req, res) => {
+  const { name, email, message } = req.body;
+
+  // Create a transport object with your email service provider details
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'nperis27@gmail.com',
+      pass: 'vuhdmdolawntwogm', // Use the app password here
+    },
+  });
+
+  // Configure the email options
+  const mailOptions = {
+    from: '${email}',
+    to: 'nperis27@gmail.com',
+    subject: `New Message from ${name}`,
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+  
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
+// Serve the index.html file for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Serve the projects.html file for /projects URL
+app.get('/projects', (req, res) => {
+  res.sendFile(path.join(__dirname, 'projects.html'));
+});
+
+// Serve the contact.html file for /contact URL
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
