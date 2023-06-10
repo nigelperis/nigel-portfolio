79cd203c9a20149264378eea3dc3d67556575c88
Nigel Peris <nperis27@gmail.com>
2023-06-10 11:27:02 +0530

    did final changes

--- script.js
+++ script.js
@@ -1,67 +1,63 @@
-function loadProjects() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "projects.html", true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        document.getElementById("content").innerHTML = xhr.responseText;
-      }
-    };
-    xhr.send();
-  }
-  
-  function loadContactForm() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "contact.html", true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        document.getElementById("content").innerHTML = xhr.responseText;
-      }
-    };
-    xhr.send();
-  }
+function loadContent(url, targetId) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      document.getElementById(targetId).innerHTML = xhr.responseText;
+    }
+  };
+  xhr.send();
+}
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const projectsContainer = document.querySelector('#projectsContainer');
-  
-    // Array of projects
-    const projects = [
-      {
-        image: '/images/proj1.jpg',
-        title: 'Vegetable Disease Detection',
-        description: 'Developed a web app with a CNN model using Machine Learning to classify Tomato and Corn Images into 12 diseases and identify their health status',
-        githubLink: 'https://github.com/nigelperis/vegetable-disease-detection'
-      },
-      {
-        image: '/images/proj2.jpg',
-        title: 'Portfolio Site for Ganesh Designmatics Ltd',
-        description: 'Developed a web app with visually appealing gallery, contact pages, and user login functionality with secure email storage.',
-        githubLink: 'https://github.com/nigelperis/SiteForGaneshDesignmaticsLtd'
-      }
-    ];
-    
-    projects.forEach(project => {
-      const projectElement = document.createElement('div');
-      projectElement.classList.add('project');
-      projectElement.innerHTML = `
-        <img src="${project.image}" alt="${project.title}">
-        <h2>${project.title}</h2>
-        <p>${project.description}</p>
-      `;
-      projectElement.addEventListener('click', () => {
-        window.location.href = project.githubLink;
-      });
-      projectsContainer.appendChild(projectElement);
+function loadProjects() {
+  loadContent("projects.html", "content");
+}
+
+function loadContactForm() {
+  loadContent("contact.html", "content");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const projectsContainer = document.querySelector("#projectsContainer");
+
+  // Array of projects
+  const projects = [
+    {
+      image: "/images/proj1.jpg",
+      title: "Vegetable Disease Detection",
+      description: "Developed a web app with a CNN model using Machine Learning to classify Tomato and Corn Images into 12 diseases and identify their health status",
+      githubLink: "https://github.com/nigelperis/vegetable-disease-detection"
+    },
+    {
+      image: "/images/proj2.jpg",
+      title: "Portfolio Site for Ganesh Designmatics Ltd",
+      description: "Developed a web app with visually appealing gallery, contact pages, and user login functionality with secure email storage.",
+      githubLink: "https://github.com/nigelperis/SiteForGaneshDesignmaticsLtd"
+    }
+  ];
+
+  projects.forEach(project => {
+    const projectElement = document.createElement("div");
+    projectElement.classList.add("project");
+    projectElement.innerHTML = `
+      <img src="${project.image}" alt="${project.title}">
+      <h2>${project.title}</h2>
+      <p>${project.description}</p>
+    `;
+    projectElement.addEventListener("click", () => {
+      window.location.href = project.githubLink;
     });
-    
+    projectsContainer.appendChild(projectElement);
   });
-  // Smooth scrolling
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+});
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
     });
   });
-  
+});
--- server.js
+++ server.js
@@ -6,25 +6,20 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-// Middleware to parse the request body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '')));
 
-// POST endpoint for the contact form submission
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'nperis27@gmail.com',
+    pass: 'vuhdmdolawntwogm',
+  },
+});
+
 app.post('/contact', (req, res) => {
   const { name, email, message } = req.body;
-
-  // Create a transport object with your email service provider details
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'nperis27@gmail.com',
-      pass: 'vuhdmdolawntwogm', // Use the app password here
-    },
-  });
-
-  // Configure the email options
   const mailOptions = {
     from: email,
     to: 'nperis27@gmail.com',
@@ -32,39 +27,37 @@ app.post('/contact', (req, res) => {
     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
   };
 
-  // Send the email
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error(error);
       res.status(500).send('Error sending email');
     } else {
       console.log('Email sent: ' + info.response);
-      res.redirect('/success'); // Redirect to success.html
+      res.redirect('/success');
     }
   });
 });
 
-// Serve the index.html file for the root URL
+const serveFile = (res, filename) => {
+  res.sendFile(path.join(__dirname, filename));
+};
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  serveFile(res, 'index.html');
 });
 
-// Serve the projects.html file for /projects URL
 app.get('/projects', (req, res) => {
-  res.sendFile(path.join(__dirname, 'projects.html'));
+  serveFile(res, 'projects.html');
 });
 
-// Serve the contact.html file for /contact URL
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, 'contact.html'));
+  serveFile(res, 'contact.html');
 });
 
-// Serve the success.html file for /success URL
 app.get('/success', (req, res) => {
-  res.sendFile(path.join(__dirname, 'success.html'));
+  serveFile(res, 'success.html');
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
