935e73b4ce9a1aceff69cc83a6cfa9f6d1541d47
Nigel Peris <nperis27@gmail.com>
2023-06-10 11:47:22 +0530

    added express

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
 const path = require('path');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const express = require('express');
 
 const app = express();
 const port = 3000;
